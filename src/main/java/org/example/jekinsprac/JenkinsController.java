ea802a0952f8a699a79de9f072f099831a60227d
ashd89 <jyjhlee0809@gmail.com>
2024-08-23 16:14:01 +0900

    "24"

--- src/main/java/org/example/jekinsprac/JenkinsController.java
+++ src/main/java/org/example/jekinsprac/JenkinsController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class JenkinsController {
     @GetMapping()
     public String saveTest() {
-        return "jenkins test: 22try";
+        return "jenkins test: 24try";
     }
 }
