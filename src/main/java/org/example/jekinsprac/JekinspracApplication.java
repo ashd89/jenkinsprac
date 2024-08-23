f7c026b3697d2e5b10928a23e1783e86a8329225
ashd89 <jyjhlee0809@gmail.com>
2024-08-23 10:18:56 +0900

    first commit

---
+++ src/main/java/org/example/jekinsprac/JekinspracApplication.java
@@ -0,0 +1,13 @@
+package org.example.jekinsprac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JekinspracApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JekinspracApplication.class, args);
+    }
+
+}
---
+++ src/main/java/org/example/jekinsprac/JenkinsController.java
@@ -0,0 +1,13 @@
+package org.example.jekinsprac;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@RequestMapping("/test")
+public class JenkinsController {
+    @GetMapping()
+    public String saveTest() {
+        return "jenkins test";
+    }
+}
